a5e07207f072d42e56d8d3b5d7bd4fae94f0a9e3
beesan-ahmad <abumfarreh@gmail.com>
2023-11-26 21:38:38 +0200

    connect NestJS app to a MongoDB server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot('mongodb+srv://abumfarrehb:9cSZK*f92R$useK@cluster0.e4ymxbb.mongodb.net/nestjs-demo?retryWrites=true&w=majority'
+
+  ),
+],
   controllers: [AppController],
   providers: [AppService],
+  
 })
 export class AppModule {}

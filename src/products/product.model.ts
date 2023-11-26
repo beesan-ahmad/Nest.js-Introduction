a4b3df274cdf420bf763a4cf5f4f7dc09345d65d
beesan-ahmad <abumfarreh@gmail.com>
2023-11-26 22:00:50 +0200

    import mongoose schema in module class

--- src/products/product.model.ts
+++ src/products/product.model.ts
@@ -1,6 +1,6 @@
 import * as mongoose from 'mongoose';
 //create a schema as a blueprint to the model
-export const productShema = new mongoose.Schema({
+export const productSchema = new mongoose.Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
     price: {type: Number, required: true},
